dfbf17f7b0803794caca5e9be740a5cff0c7f837
Ian <ballbeian@icloud.com>
2026-09-10 21:43:10 +0200

    Auto detect the network to scan when using -n flag.

--- Network Scanner/scanner.py
+++ Network Scanner/scanner.py
@@ -7,12 +7,24 @@ import socket
 def address():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="Target IP Address/Adresses.")
-    parser.add_argument("-n", "--network", dest="network", help="Target network, x.x.x")
+    parser.add_argument("-n", "--network", dest="network", nargs="?", const="auto",
+                         help="Target network, x.x.x. Omit the value (just pass -n) to auto-detect.")
     options = parser.parse_args()
 
     if not options.target and not options.network:
         parser.error("[-] Please specify and IP Address or Addresses, use --help for more info.")
     return options
+
+def get_local_prefix():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    prefix = ".".join(local_ip.split(".")[:3])
+    return prefix
 
 def scan(ip):
     icmp = IP(dst=ip)/ICMP()
@@ -30,7 +42,7 @@ def scan_network(prefix):
     packet = broadcast / arp_request
 
     print(f"Scanning range {range}")
-    answered, _ = srp(packet, timeout=2, verbose=False, iface="Ethernet 3")
+    answered, _ = srp(packet, timeout=2, verbose=False)
 
     print("\n--- Active Hosts Found ---")
     for sent, received in answered:
@@ -43,4 +55,5 @@ options = address()
 if options.target:
     single_output = scan(options.target)
 elif options.network:
-    network_output = scan_network(options.network)
+    prefix = get_local_prefix() if options.network == "auto" else options.network
+    network_output = scan_network(prefix)
