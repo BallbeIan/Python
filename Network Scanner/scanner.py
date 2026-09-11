04e8fd55ced757616c96a61337dd8e50ce086f19
Ian <ballbeian@icloud.com>
2026-09-11 09:24:07 +0200

    Adding option to scan a specific range.

--- Network Scanner/scanner.py
+++ Network Scanner/scanner.py
@@ -9,10 +9,12 @@ def address():
     parser.add_argument("-t", "--target", dest="target", help="Target IP Address/Adresses.")
     parser.add_argument("-n", "--network", dest="network", nargs="?", const="auto",
                          help="Target network, x.x.x. Omit the value (just pass -n) to auto-detect.")
+    parser.add_argument("-r", "--range", dest="range", help="Set the range of ip's separated by - x.x.x.x-x.x.x.x")
     options = parser.parse_args()
 
     if not options.target and not options.network:
         parser.error("[-] Please specify and IP Address or Addresses, use --help for more info.")
+    
     return options
 
 def get_local_prefix():
@@ -24,6 +26,7 @@ def get_local_prefix():
         s.close()
 
     prefix = ".".join(local_ip.split(".")[:3])
+
     return prefix
 
 def scan(ip):
@@ -35,18 +38,21 @@ def scan(ip):
         print("Target is up")
         
 def scan_network(prefix):
-    range = f"{prefix}.0/24"
+    network = f"{prefix}.0/24"
 
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request = ARP(pdst=range)
+    arp_request = ARP(pdst=network)
     packet = broadcast / arp_request
 
-    print(f"Scanning range {range}")
+    print(f"Scanning network {network}")
     answered, _ = srp(packet, timeout=2, verbose=False)
 
-    print("\n--- Active Hosts Found ---")
+    print("\n--- Hosts Found ---")
     for sent, received in answered:
         print(f"IP: {received.psrc:<15} |  MAC: {received.hwsrc}")
+
+def scan_range(network):
+    
 
 #============================================================================================================
 
@@ -57,3 +63,6 @@ if options.target:
 elif options.network:
     prefix = get_local_prefix() if options.network == "auto" else options.network
     network_output = scan_network(prefix)
+elif options.range:
+    prefix = get_local_prefix()
+    range_output = scan_range(prefix)
